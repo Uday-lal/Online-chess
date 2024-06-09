d224563846b0c696a336dda1c8e03746a8891e0e
Uday lal <udaylal014@gmail.com>
2024-06-09 22:37:42 +0530

    Inititalization

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const next = require("next");
+
+const port = 3050;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.post("/api/login", (req, res) => {
+    return res.json({ user: "Uday lal" });
+  });
+  server.post("/api/create_account", (req, res) => {
+    return res.json({ user: "Uday lal" });
+  });
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
---
+++ src/app/login/page.js
@@ -0,0 +1,16 @@
+import styles from "../../styles/Login.module.css";
+
+function Login() {
+  return (
+    <>
+      <div className="w-full h-full">
+        <div className={styles.page_grid}>
+          <div className={styles.hero_image}></div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
