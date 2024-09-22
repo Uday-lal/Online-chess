1c5aca7b6838178176fae44505812de06a52d413
Uday lal <udaylal014@gmail.com>
2024-09-23 00:37:13 +0530

    Update move calculator

--- src/func/moveCalculator.js
+++ src/func/moveCalculator.js
@@ -1,27 +1,46 @@
 "use client";
 
 export class MoveCalculator {
-  constructor(posX, posY, board, keyword) {
+  constructor(posX, posY, pieceName) {
     this.posX = posX;
     this.posY = posY;
-    this.board = board;
-    this.keyword = keyword;
+    this.pieceName = pieceName;
+    this.direction = pieceName[1] === "b" ? -1 : 1;
   }
 
   calculateMoves() {
     let moves = [];
-    if (this.keyword[0] === "p") {
+    if (this.pieceName[0] === "p") {
       moves = this.#pawnMoveCalculation();
     }
     return moves;
   }
 
   #pawnMoveCalculation() {
-    // ...
+    const diagonaLeft = this.#leftDiagonalMotion(1);
+    const diagonaRight = this.#rightDiagonalMotion(1);
+    let straightMotion;
+    if (this.posX === 1 || this.posX - 8 === 1) {
+      straightMotion = this.#straightMotion(2);
+    } else {
+      straightMotion = this.#straightMotion(1);
+    }
+
+    const moves = [straightMotion, diagonaLeft, diagonaRight];
+
+    return moves;
   }
 
   #straightMotion(steps) {
-    // ...
+    let x = this.posX;
+    let stepCount = 0;
+
+    while (x < 8 && stepCount < steps) {
+      x += this.direction;
+      stepCount++;
+    }
+
+    return [x, this.posY];
   }
 
   #sideMotion(steps) {
