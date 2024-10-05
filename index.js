be7d11884bed6a57ef25e043f57d8135e22dbeab
Uday lal <udaylal014@gmail.com>
2024-10-05 21:14:06 +0530

    Add socket io

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const next = require("next");
 const cookieParser = require("cookie-parser");
-const authMiddleWare = require("./Middleware/auth");
+const http = require("http");
+const { Server } = require("socket.io");
 
 require("dotenv").config();
 
@@ -9,25 +10,41 @@ require("./Models/db");
 
 const usersRouter = require("./router/users");
 
-const port = 3050;
+const port = 8000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
+  const expressApp = express();
+  const server = http.createServer(expressApp);
 
-  server.use(express.json());
-  server.use(cookieParser());
-  // server.use(authMiddleWare);
+  const io = new Server(server);
 
-  server.use(usersRouter);
+  io.on("connection", (socket) => {
+    console.log("a user connected");
 
-  server.all("*", (req, res) => {
+    socket.on("message", (msg) => {
+      console.log("Message from client:", msg);
+      socket.emit("message", `Server received: ${msg}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+  });
+
+  expressApp.use(express.json());
+  expressApp.use(cookieParser());
+  // expressApp.use(authMiddleWare);
+
+  expressApp.use(usersRouter);
+
+  expressApp.all("*", (req, res) => {
     return handle(req, res);
   });
 
-  server.listen(port, (err) => {
+  expressApp.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
