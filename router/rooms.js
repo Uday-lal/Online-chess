39116c4970d1d6b0dc693e6711111aec2b742f15
Uday lal <udaylal014@gmail.com>
2024-10-21 22:41:33 +0530

    Add roooms router

---
+++ router/rooms.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/v1/create_room", async (req, res) => {
+  // ...
+});
+
+router.get("/v1/join_room", async (req, res) => {
+  // ...
+});
