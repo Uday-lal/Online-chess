97af6130f4686087faae95d97107e226fe73782f
Uday Lal <udaylal014@gmail.com>
2025-02-04 00:59:49 +0530

    update validation.js

--- chessFunc/Validation.js
+++ chessFunc/Validation.js
@@ -2,18 +2,14 @@ class Validation {
   /*
         Args:
             Board and Piece are pritty stright forward
-            cPosX: Mean current position X
-            cPosY: Mean current position Y
-            wPosX: Mean wishes position X 
-            wPosY: Mean wishes position Y 
+            posX: Mean wishes position X 
+            posY: Mean wishes position Y 
     */
-  constructor(board, piece, cPosx, cPosY, wPosX, wPosY) {
+  constructor(board, piece, posX, posY) {
     this.board = board;
     this.piece = piece;
-    this.cPosX = cPosX;
-    this.cPosY = cPosY;
-    this.wPosX = wPosX;
-    this.wPosY = wPosY;
+    this.posX = posX;
+    this.posY = posY;
     this.calculatedMove = [];
   }
 
